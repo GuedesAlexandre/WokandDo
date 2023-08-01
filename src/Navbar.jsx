b3dfdf9152a79d37548b8886bb89cd41c21062f1
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2023-08-01 21:30:42 +0200

    modif navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
                         <li><a href='#'>Reservation</a></li>
                         <li><a href='#'>Contact</a></li>
                     </ul>
-                    <div className="menu-icon" onClick={toggleMenu}>
+                    <div className="menu-icon"  onClick={toggleMenu}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="40px" height="40px">
                             <path d="M0 0h24v24H0z" fill="none" />
                             <path d="M3 18h18v-2H3v2zM3 6v2h18V6H3zm0 7h18v-2H3v2z" />
